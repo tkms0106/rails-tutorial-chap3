7b200df66d2b9df5f215a2ed2d81b141219281c0
tkms0106 <siberianexress1991+github@gmail.com>
2017-10-01 17:39:03 +0900

    7.3.4演習4

--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -14,5 +14,6 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'div#error_explanation'
     assert_select 'div.alert'
     assert_select 'div.alert-danger'
+    assert_select 'form[action="/signup"]'
   end
 end
