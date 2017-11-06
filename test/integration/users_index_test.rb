02f25cb28ec89b15c2acfebcdb7c18eae2c8a29c
tkms0106 <siberianexpress1991@gmail.com>
2017-11-06 21:36:55 +0900

    非管理者のユーザー一覧ページ表示に対する統合テストの修正

--- test/integration/users_index_test.rb
+++ test/integration/users_index_test.rb
@@ -27,6 +27,7 @@ class UsersIndexTest < ActionDispatch::IntegrationTest
   test "index as non-admin" do
     log_in_as(@non_admin)
     get users_path
+    assert_template 'users/index'
     assert_select 'a', text: 'delete', count: 0
   end
 end
