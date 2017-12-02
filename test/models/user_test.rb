3c78561b227e7670950ac0b5b109942d34bc1f61
tkms0106 <23391543+tkms0106@users.noreply.github.com>
2017-12-02 20:15:36 +0900

    dependent: :destroyのテストを追加

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -10,17 +10,17 @@ class UserTest < ActiveSupport::TestCase
   test "should be valid" do
     assert @user.valid?
   end
-  
+
   test "name should be present" do
     @user.name = "     "
     assert @user.invalid?
   end
-  
+
   test "email should be present" do
     @user.email = "     "
     assert @user.invalid?
   end
-  
+
   test "name should not be too long" do
     @user.name = "a" * 51
     assert @user.invalid?
@@ -30,7 +30,7 @@ class UserTest < ActiveSupport::TestCase
     @user.email = "a" * 244 + "@example.com"
     assert @user.invalid?
   end
-  
+
   test "email validation should accept valid addresses" do
     valid_addresses = %w[user@example.com USER@foo.COM A_US-ER@foo.bar.org
                          first.last@foo.jp alice+bob@baz.cn]
@@ -39,7 +39,7 @@ class UserTest < ActiveSupport::TestCase
       assert @user.valid?, "#{valid_address.inspect} should be valid"
     end
   end
-  
+
   test "email validation should reject invalid addresses" do
     invalid_addresses = %w[user@example,com user_at_foo.org user.name@example.
                            foo@bar_baz.com foo@bar+baz.com foo@bar..com]
@@ -48,21 +48,21 @@ class UserTest < ActiveSupport::TestCase
       assert @user.invalid?, "#{invalid_address.inspect} should be invalid"
     end
   end
-  
+
   test "email addresses should be unique" do
     duplicate_user = @user.dup
     duplicate_user.email = @user.email.upcase
     @user.save
     assert duplicate_user.invalid?
   end
-  
+
   test "email addresses should be saved as lower-case" do
     mixed_case_email = "Foo@ExAMPle.CoM"
     @user.email = mixed_case_email
     @user.save
     assert_equal mixed_case_email.downcase, @user.reload.email
   end
-  
+
   test "password should be present (nonblank)" do
     @user.password = @user.password_confirmation = " " * 6
     assert @user.invalid?
@@ -72,8 +72,16 @@ class UserTest < ActiveSupport::TestCase
     @user.password = @user.password_confirmation = "a" * 5
     assert @user.invalid?
   end
-  
+
   test "authenticated? should return false for a user with nil digest" do
     assert_not @user.authenticated?(:remember, '')
+  end
+
+  test "associated microposts should be destroyed" do
+    @user.save
+    @user.microposts.create!(content: "Lorem ipsum")
+    assert_difference 'Micropost.count', -1 do
+      @user.destroy
+    end
   end
 end
